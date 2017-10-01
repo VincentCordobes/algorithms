9aa404ffb311334be47b2c1b58e69b0db42fbf96
VincentCordobes <vincent.cordobes@gmail.com>
2017-10-01 11:22:37 +0200

    better MiniMaxSum

--- warmup/MiniMaxSum.fs
+++ warmup/MiniMaxSum.fs
@@ -1,14 +1,11 @@
 open System
 
-
 let solve numbers =
-  let sum = List.sum numbers
-  List.fold (fun (min, max) e ->
-    let tempSum = sum - e
-    let tempMin = if tempSum < min then tempSum else min
-    let tempMax = if tempSum > max then tempSum else max 
-    (tempMin, tempMax)
-  ) (Int64.MaxValue, Int64.MinValue) numbers
+  let sorted = List.sort numbers
+  let min = sorted |> List.rev |> List.tail |> List.sum
+  let max = sorted |> List.tail |> List.sum
+  (min, max)
+
 
 [<EntryPoint>]
 let main argv = 
@@ -19,5 +16,3 @@ let main argv =
     |> solve
     |> function min, max -> printfn "%d %d" min max
   0
-
-solve [1L; 2L; 3L; 4L; 5L]
